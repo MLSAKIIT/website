375cdf608bdf5c89f3876c5bfb8b0aeadab1792d
Vaibhav Raj <vaibhavmonu8309@gmail.com>
2025-05-29 16:07:21 +0530

    feat: setup media uploads

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20250529_024145_initial from './20250529_024145_initial';
+import * as migration_20250529_103541_media_uploads from './20250529_103541_media_uploads';
 
 export const migrations = [
   {
     up: migration_20250529_024145_initial.up,
     down: migration_20250529_024145_initial.down,
-    name: '20250529_024145_initial'
+    name: '20250529_024145_initial',
+  },
+  {
+    up: migration_20250529_103541_media_uploads.up,
+    down: migration_20250529_103541_media_uploads.down,
+    name: '20250529_103541_media_uploads'
   },
 ];
